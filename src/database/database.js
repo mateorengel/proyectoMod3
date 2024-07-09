c641e5d46f9e7bb528f3216b399fba4276ff5a2c
mateorengel <mateorengel@gmail.com>
2024-07-08 21:31:52 -0400

    das

--- src/database/database.js
+++ src/database/database.js
@@ -9,5 +9,11 @@ process.env.DB_PASSWORD,//password
     host:process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     logging: console.log,
+    dialectOptions:{
+        ssl:{
+            require:true,
+            rejectUnauthorized:false
+        }
+    }
 }
 );
